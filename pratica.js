6b6a93fcdd50b4ff586b82c9f242d7192082bb8a
unknown <wesleydasilvaalmeida706@gmail.com>
2025-10-23 14:08:48 -0300

    pratica

--- pratica.js
+++ pratica.js
@@ -1,10 +1,41 @@
-let a = 9
-let b = 8
-let c = 4
-let soma = a + b
-if (soma > c) {
-    alert("a soma é maior que C")
-}
-else {
-    alert("soma é menor")
-}
+//1
+// let a = prompt("digite o numero 1")
+// let b = prompt("digite o numero 2")
+// let c = prompt("digite o numero 3")
+// let soma = a + b
+// if (soma > c) {
+//     alert("a soma é maior que C")
+// }
+// else {
+//     alert("soma é menor")
+// }
+
+//2 
+// let numero = prompt("digite o numero")
+// if (numero % 2 == 0) {
+//     alert('numero par')
+// }
+// else {
+//     alert("numero impar")
+// }
+// if (numero > 0) {
+//     alert("numero positivo")
+// }
+// else{
+//     alert("numero negativo")
+// }
+
+//3
+// let a = parseInt(prompt("digite o numero 1"))
+// let b = parseInt(prompt("digite o numero 2"))
+// let resultado = 0
+// if (a == b) {
+//     resultado = a + b
+//     alert(resultado)
+// }
+// else {
+//     resultado = a * b
+//     alert(resultado)
+// }
+
+
